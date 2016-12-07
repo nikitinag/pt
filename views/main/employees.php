a1eaf5deeec33d6e3dbd062e8143b862701bf012
nikitinag <forallsites2015@gmail.com>
2016-12-07 18:05:01 +0300

    END 02

--- views/main/employees.php
+++ views/main/employees.php
@@ -7,11 +7,13 @@ use yii\helpers\Html;
     <h3>Вакансии</h3>
     <p><?=$head?></p>
     <div class="employees">
+        <?php if($list): ?>
         <? foreach($list as $employ): ?>
            <ul>
            <h4><?=$employ['name']?></h4>
            <p><i>Дата выставления <?=formatDate($employ['date'])?></i><span><?= Html::a('просмотреть...', ['/employees', 'id' => $employ['id']]) ?></span></p>
            </ul>
         <? endforeach; ?>
+        <?php endif; ?>
     </div>
 </div>
